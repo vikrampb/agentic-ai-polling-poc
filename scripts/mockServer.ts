09716fd4a7b10474d91022ca3184f02bca91bddd
Vikram Bhatwadekar <vbhatwadekar@collineargroup.com>
2026-07-13 12:00:14 -0400

    demo: restore server message

--- scripts/mockServer.ts
+++ scripts/mockServer.ts
@@ -64,7 +64,7 @@ app.get('/api/login', (req: Request, res: Response) => {
 
   return res.status(200).json({
     success:      true,
-    message:      'Login successful. BROKEN!',
+    message:      'Login successful. Welcome!',
     exportStatus: user.export_status,
   });
 });
